7ef31101cf4bd9779ad2fe0f2df2cf172a469161
Bhavik Patel <bhavik252patel@gmail.com>
2022-10-11 18:40:23 +0530

    index.js clean require

--- controllers/price_grid.controller.js
+++ controllers/price_grid.controller.js
@@ -12,65 +12,27 @@ export const gridcreate = async (req, res) => {
 
   try {
     const check = await User.findOne({ _id: data.user_id });
-    //const scheck = await strategy.findOne({ _id: data.strategy_id });
-    //const symbol_id = scheck.symbol_id;
-    //const symcheck = await symbol.findOne({ _id: symbol_id });
     const binance = new Binance().options({
       APIKEY: check.API_Key,
       APISECRET: check.API_Secret,
       useServerTime: true, //binance-api/node_modules/node-binance-api/node-binance-api.js/default_options/recvWindow //default: 5000
       test: true,
       verbose: true,
-      //AutoTimestamp: true,
       recvWindow: 60000,
     });
 
-    function balance_update(data) {
-      console.log("Balance Update");
-      for ( let obj of data.B ) {
-        let { a:asset, f:available, l:onOrder } = obj;
-        if ( available == "0.00000000" ) continue;
-        console.log(asset+"\tavailable: "+available+" ("+onOrder+" on order)");
-      }
-    }
-    function execution_update(data) {
-      let { executionType, s:symbol, p:price, q:quantity, S:side, o:orderType, i:orderId, X:orderStatus } = data;
-      if ( executionType == "NEW" ) {
-        if ( orderStatus == "REJECTED" ) {
-          console.log("Order Failed! Reason: "+data.r);
-        }
-        console.log(symbol+" "+side+" "+orderType+" ORDER #"+orderId+" ("+orderStatus+")");
-        console.log("..price: "+price+", quantity: "+quantity);
-        return;
-      }
-      //NEW, CANCELED, REPLACED, REJECTED, TRADE, EXPIRED
-      console.log(symbol+"\t"+side+" "+executionType+" "+orderType+" ORDER #"+orderId);
-    }
-    execution_update();
-    binance.websockets.userData(execution_update());
-    /*
-    binance.websockets.trades(['BTCUSDT'], (trades) => {
-      let {e:eventType, E:eventTime, s:symbol, p:price, q:quantity, m:maker, a:tradeId} = trades;
-      console.info(symbol+" trade update. price: "+price+", quantity: "+quantity+", maker: "+maker);
-      clear();
-    });
-
-*/
     const order = [];
-    //order.length = scheck.margin_call_limit;
     for (var i = 0; i < data.open_call.length; i++) {
       const quantity = (
         data.open_call[i].margin_buy_call / data.open_call[i].margin_call
       ).toFixed(2);
       const leverage = data.open_call[i].leverage;
-      //const refinedqty = quantity * data.open_call[i].leverage;
-      //console.log(quantity);
       const r = await binance.futuresLeverage(
         data.symbol,
         data.open_call[i].leverage
       );
       console.log(r);
-      
+
       const obj = {
         symbol: data.symbol,
         side: data.position_side,
@@ -81,7 +43,7 @@ export const gridcreate = async (req, res) => {
         timeInForce: "GTC",
       };
       order.push(obj);
-      
+      let timeInForce = "GTC";
       const place = await binance.futuresBuy(
         data.symbol,
         quantity,
--- index.js
+++ index.js
@@ -10,8 +10,8 @@ import grid from "./routes/grid.route.js";
 import symbol from "./routes/symbol.route.js";
 import admin from "./admin/route.js";
 import strategy from "./routes/strategy.route.js";
-import request from "request";
 import WebSocket from "ws";
+import crypto from "crypto";
 
 export const binance = new Binance().options({
   APIKEY: "0d1e94b104dd54fde98dec9a83f8916b1af3daa0c81c8c754b59ce3d62c8a00a",
@@ -28,7 +28,108 @@ const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname + `public`)));
 app.set("views", path.join(__dirname, "views"));
 app.set(`view engine`, `ejs`);
+/*
+const key = await crypto.subtle.importKey(
+  "raw",
+  toBytes(secretKey),
+  { name: "HMAC", hash: "SHA-256" },
+  true,
+  ["sign"]
+);
+const timestamp = Date.now();
+const queryString = `timestamp=${timestamp}`;
+const signature = await crypto.subtle.sign("HMAC", key, toBytes(queryString));
+const response = await fetch(
+  `/api/v3/account?${queryString}&signature=${toHex(signature)}`,
+  { headers: { "X-MBX-APIKEY": apiKey } }
+);
+console.log(await response.json());
+*/
+/*
+const connection = new WebSocket(
+  "wss://stream.binance.com/ws/BudumoYTTCcSNHXwNvkH5ZkAWPIBIf6Tuoc8zIY7E8ylxIUYDSeJ2cCGtnqpbOct"
+);
 
+connection.onopen = () => {
+  connection.send(
+    JSON.stringify([
+      {
+        e: "ORDER_TRADE_UPDATE",
+        method: "SUBSCRIBE",
+        id: 1,
+      },
+    ])
+  );
+  console.log("Connected to Data Streaming");
+};
+
+connection.onmessage = async (ev) => {
+  //let tickers = JSON.parse(ev.method);
+  /*for (const ticker of tickers) {
+    if (ticker.symbol.endsWith("USDT")) {
+      data[ticker.symbol] = ticker;
+    }
+  }
+  //console.log("Tickers loaded:", Object.keys(data).length);
+  try {
+    console.log(ev._url);
+  } catch (error) {
+    console.log(error.message);
+  }
+  //console.log({ ...ev });
+};
+
+connection.onclose = (err) => {
+  console.log("disconnected");
+};
+*/
+/*
+async function testStreaming() {
+  const stream = new WebSocket(
+    "wss://fstream.binance.com/ws/tc8i8GejclkJOVCVtUASyCgggXTqOcjVQLRUR3UwCPZbMMOEyjIcBm8qwSH4aJtz"
+  );
+  //console.log(stream)
+  stream.onmessage = async (ev) => {
+    let tickers = JSON.parse(ev.data).map(
+      ({ e, E, T, o, s, c, S, f, q, p, ap, x, X, i, ps }) => ({
+        eventType: ORDER_TRADE_UPDATE,
+        symbol: s,
+        clientOrderId: c,
+        side: S,
+        timeInForce: f,
+        orderStatus: X,
+        orderId: i,
+        positionSide: ps,
+      })
+    );
+    console.log(tickers.c);
+    /*
+    for (const ticker of tickers) {
+      if (ticker.symbol.endsWith("BUSD")) {
+        data[ticker.symbol] = ticker;
+      }
+    }
+    console.log("Tickers loaded:", Object.keys(data).length);
+    */
+/*if (Date.now() - startDate > 10000) {
+      stream.close()
+      const selectedTickers = Object.values(data)
+        .sort(
+          (a, b) =>
+            b.totalTradedQuoteAssetVolume - a.x
+        )
+        .slice(0, 5)
+        console.log(selectedTickers);
+        await Promise.all(
+          selectedTickers.map(async (ticker) => {
+            const update = await orderTradeUpdate(ticker.symbol);
+            console.log(`${ticker.symbol}:`, update);
+          })
+          );
+        };
+      }
+      testStreaming();
+      */
 /*s
 binance.websockets.depth(["BTCUSDT"], (depth) => {
   let { b: bidDepth } = depth;
@@ -44,6 +145,42 @@ const result = async (req, res) => {
 };
 result();
 */
+/*
+binance.futuresSubscribe(
+  "wss://stream.binance.com:9443/ws/tc8i8GejclkJOVCVtUASyCgggXTqOcjVQLRUR3UwCPZbMMOEyjIcBm8qwSH4aJtz",
+  console.log()
+);
+
+console.log(binance.futuresSubscriptions());*/
+/*
+binance.allOrders("BTC", (error, orders, symbol) => {
+  console.info(symbol + " orders:", orders);
+});
+binance.websockets.userData(
+  "wss://stream.binance.com:9443/ws/tc8i8GejclkJOVCVtUASyCgggXTqOcjVQLRUR3UwCPZbMMOEyjIcBm8qwSH4aJtz",
+  (trades) => {
+    let {
+      e: listStatus,
+      E: eventTime,
+      s: symbol,
+      l: statusType,
+      L: orderStatus,
+      r: rejectReason,
+      a: tradeId,
+    } = trades;
+    console.info(
+      symbol +
+        " trade update. price: " +
+        price +
+        ", quantity: " +
+        quantity +
+        ", maker: " +
+        maker
+    );
+  }
+);
+
+*/
 const r = async (req, res) => {
   const s = await binance.futuresGetDataStream();
   //const s = await binance.futuresBuy("BTCUSDT", 0.1, 19100);
@@ -55,109 +192,27 @@ const r = async (req, res) => {
 r();
 
 /*
-const client = new ws("wss://fstream.binance.com/ws/fCfAS89zQJ7yY38xzEne3mbDVq2UREWsJW5EktL3jWhdOLqtED2qWgbhZ06EvZMG")
-client.on('open', () => {
-  client.send('hi')
-})
-const DATA_STREAM_ENDPOINT = "wss://stream.binance.com:9443/ws";
-const BINANCE_API_ROOT = "https://api.binance.com";
-const LISTEN_KEY_ENDPOINT = `${BINANCE_API_ROOT}/api/v1/userDataStream`;
-
-const fetchAccountWebsocketData = async () => {
-  //const listenKey = await fetchListenKey();
-  
-  //console.log("-> ", listenKey); // valid key is returned
-  
-  let ws;
-  
-  try {
-    ws = new WebSocket(
-      `${DATA_STREAM_ENDPOINT}/"fCfAS89zQJ7yY38xzEne3mbDVq2UREWsJW5EktL3jWhdOLqtED2qWgbhZ06EvZMG"`
-      );
-      ws.onmessage("message", (data) => {
-        let stockObject = JSON.parse(data);
-        console.log(stockObject.p);
-      })
-      ws.onmessage("outboundAccountInfo", (accountData) => console.log(accountData));
-    } catch (err) {
-      console.log(err)
-      //throw `ERROR - fetchAccountWebsocketData: ${err}`;
-    }
-    
-    // Nothing returns from either
-  };
-  /*
-  const openWebSocket = (endpoint) => {
-    const p = new Promise((resolve, reject) => {
-      const ws = new WebSocket(endpoint);
-      
-      console.log("\n-->> New Account Websocket");
-      
-      ws.on(
-        "open",
-        () => {
-          console.log("\n-->> Websocket Account open...");
-          resolve(ws);
-        },
-        (err) => {
-          console.log("fetchAccountWebsocketData error:", err);
-          reject(err);
-        }
-        );
-      });
-      
-      p.catch((err) => console.log(`ERROR - fetchAccountWebsocketData: ${err}`));
-      return p;
-    };
-    
-    const fetchListenKey = () => {
-      const p = new Promise((resolve, reject) => {
-        const options = {
-          url: LISTEN_KEY_ENDPOINT,
-          headers: {
-            "X-MBX-APIKEY":
-            "0d1e94b104dd54fde98dec9a83f8916b1af3daa0c81c8c754b59ce3d62c8a00a",
-          },
-        };
-        
-        request.post(options, (err, httpResponse, body) => {
-          if (err) return reject(err);
-          
-          resolve(JSON.parse(body).listenKey);
-        });
-      });
-      
-      p.catch((err) => console.log(`ERROR - fetchListenKey: ${err}`));
-      return p;
-    };
-    openWebSocket();
-    fetchListenKey();
-    
-    /*
-    
-    const data = async (req, res) => {
-      let position_data = await binance.futuresPositionRisk(),
-      markets = Object.keys(position_data);
-      for (let market of markets) {
-        let obj = position_data[market],
-        size = Number(obj.positionAmt);
-        if (size == 0) continue;
-        console.info(`${leverage}x\t${market}\t${obj.unRealizedProfit}`);
-      }
-    };
-    app.get("/", (req, res) => {
-      binance.websockets.depth(["BTCUSDT"], (depth) => {
-        let { b: bidDepth } = depth;
-        const s = JSON.parse(bidDepth[0][0]).toFixed(1);
-        res.render("pages/design-crypto/main/smart-single", {
-          BTC: s,
-          Bal: result(),
-        });
-      });
+const data = async (req, res) => {
+  let position_data = await binance.futuresPositionRisk(),
+    markets = Object.keys(position_data);
+  for (let market of markets) {
+    let obj = position_data[market],
+      size = Number(obj.positionAmt);
+    if (size == 0) continue;
+    console.info(`${leverage}x\t${market}\t${obj.unRealizedProfit}`);
+  }
+};
+app.get("/", (req, res) => {
+  binance.websockets.depth(["BTCUSDT"], (depth) => {
+    let { b: bidDepth } = depth;
+    const s = JSON.parse(bidDepth[0][0]).toFixed(1);
+    res.render("pages/design-crypto/main/smart-single", {
+      BTC: s,
+      Bal: result(),
     });
-    fetchAccountWebsocketData();
-    */
-   
+  });
+});
+*/
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
@@ -211,21 +266,26 @@ let orders = [
   },
 ];
 console.info(await binance.futuresMultipleOrders(orders));
-*/
-/*
-binance.websockets.depthCache(["BTCUSDT"], (symbol, depth) => {
-  let bids = binance.sortBids(depth.bids);
-  let asks = binance.sortAsks(depth.asks);
-  console.info(symbol + " depth cache update");
-  console.info("bids", bids);
-  console.info("asks", asks);
-  console.info("best bid: " + binance.first(bids));
-  console.info("best ask: " + binance.first(asks));
-  console.info("last updated: " + new Date(depth.eventTime));
-});
-*/
-//binance.websockets.bookTickers("BTCUSDT", console.log);
 
+binance.websockets.userData(["ORDER_TRADE_UPDATE"], (orderId) => {
+  console.log(orderId);
+  //let bids = binance.sortBids(depth.bids);
+  //let asks = binance.sortAsks(depth.asks);
+  //console.info(symbol + " depth cache update");
+  //console.info("bids", bids);
+  //console.info("asks", asks);
+  //console.info("best bid: " + binance.first(bids));
+  //console.info("best ask: " + binance.first(asks));
+  //console.info("last updated: " + new Date(depth.eventTime));
+});
+/*
+const stream = binance.websockets.userFutureData(
+  (balance_update, execution_update) => {
+    console.log(execution_update);
+  }
+  );
+  console.log(stream);
+  */
 /*
 const BTC = binance.websockets.miniTicker((markets) => {
   console.info(markets.BTCUSDT);
